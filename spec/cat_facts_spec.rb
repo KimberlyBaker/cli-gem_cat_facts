6299efc376ac664c53486bb5299fd24f8074defe
Kimberly <bakerk2010@gmail.com>
2021-03-16 23:13:06 -0400

    Started files for CLI Gem

---
+++ spec/cat_facts_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe CatFacts do
+  it "has a version number" do
+    expect(CatFacts::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
